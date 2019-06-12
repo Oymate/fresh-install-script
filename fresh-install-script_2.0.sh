5b50e6ff889a45b84de21368c410f81e2c174c84
Aaron Honeycutt <honeycuttaaron3@gmail.com>
2019-06-12 14:37:32 -0600

    reorder and update paths

--- fresh-install-script_2.0.sh
+++ fresh-install-script_2.0.sh
@@ -10,6 +10,47 @@ echo "------------------------------------------------------------------"
 sleep 2
 clear
 
+echo "--------------------------------"
+echo "Mounting your Backup"
+echo "--------------------------------"
+
+sudo mount /media/$uservar/Backup
+sudo mount /dev/sda1 /media/$uservar/Backup
+
+echo "---------------------------------"
+echo "Restoring files from Backup"
+echo "---------------------------------"
+
+# Setting username for path logic
+read -p 'Username: ' uservar
+clear
+
+# Setting location
+read -p 'Mount: ' mountvar
+clear
+
+tar -xzvf /media/$uservar/Backup/Pictures.tar.gz
+cp -r home/aaronhoneycutt/Pictures/* /home/$uservar/Pictures/
+
+tar -xzvf /media/$uservar/Backup/Documents.tar.gz
+cp -r home/aaronhoneycutt/Documents/* /home/$uservar/Documents/
+
+tar -xzvf /media/$uservar/Backup/config-files.tar.gz 
+cp -r home/aaronhoneycutt/.config/* /home/$uservar/.config/
+
+echo "---------------------------------------------"
+echo "Would you like to install some applications?"
+echo "#1 will exit the script and #2 will continue"
+echo "---------------------------------------------"
+echo -n "Enter choice: "; read appchoice
+case "$appchoice" in
+1) exit 1
+    ;;
+2) echo "Continuing";;
+ esac
+ sleep 1
+ clear
+ 
 # Updating
 # -------------
 echo "--------------------------------------------"
@@ -29,47 +70,6 @@ echo "-------------------------------"
 sudo -k apt upgrade
 sleep 2
 clear
-
-echo "--------------------------------"
-echo "Mounting your ExtraDrive1"
-echo "--------------------------------"
-
-sudo mount /media/$uservar/ExtraDrive1
-sudo mount /dev/sda1 /media/$uservar/ExtraDrive1
-
-echo "---------------------------------"
-echo "Restoring files from ExtraDrive1"
-echo "---------------------------------"
-
-# Setting username for path logic
-read -p 'Username: ' uservar
-clear
-
-# Setting location
-read -p 'Mount: ' mountvar
-clear
-
-tar -xzvf /media/$uservar/ExtraDrive1/Backups/Pictures.tar.gz
-cp -r home/aaronhoneycutt/Pictures/* /home/$uservar/Pictures/
-
-tar -xzvf /media/$uservar/ExtraDrive1/Backups/Documents.tar.gz
-cp -r home/aaronhoneycutt/Documents/* /home/$uservar/Documents/
-
-tar -xzvf /media/$uservar/ExtraDrive1/Backups/config-files.tar.gz 
-cp -r home/aaronhoneycutt/.config/* /home/$uservar/.config/
-
-echo "---------------------------------------------"
-echo "Would you like to install some applications?"
-echo "#1 will exit the script and #2 will continue"
-echo "---------------------------------------------"
-echo -n "Enter choice: "; read appchoice
-case "$appchoice" in
-1) exit 1
-    ;;
-2) echo "Continuing";;
- esac
- sleep 1
- clear
  
 # Communication Tools
 # --------------------
